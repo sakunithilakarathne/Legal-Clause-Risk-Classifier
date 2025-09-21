071cd7163ae9b056ac412423fd5a268cd38b0f5a
Sakuni Thilakarathne <scsthilakarathne@gmail.com>
2025-09-21 22:03:06 +0530

    Add congif folder with all file paths

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+# Base directories
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ARTIFACTS_DIR = os.path.join(BASE_DIR, "artifacts")
+LOGS_DIR = os.path.join(BASE_DIR, "logs")
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+# Raw Dataset
+RAW_DATA_DIR_PATH = os.path.join(DATA_DIR,"raw")
+RAW_DATASET_PATH = os.path.join(RAW_DATA_DIR_PATH,"CUAD_v1.json")
+
+# Processed Dataset
+PROCESSED_DATA_DIR_PATH = os.path.join(DATA_DIR,"processed")
+NORMALIZED_DATASET_PATH = os.path.join(PROCESSED_DATA_DIR_PATH,"normalized_dataset.csv")
+EXTRACTED_CLAUSES_DATASET_PATH = os.path.join(PROCESSED_DATA_DIR_PATH,"extracted_clauses.csv")
+FINAL_CLAUSES_DATASET_PATH = os.path.join(PROCESSED_DATA_DIR_PATH,"final_clauses.csv")
+TRAIN_CLAUSES_WITH_MULTI_HOT_PATH = os.path.join(PROCESSED_DATA_DIR_PATH,"train_with_mh.csv")
+TEST_CLAUSES_WITH_MULTI_HOT_PATH = os.path.join(PROCESSED_DATA_DIR_PATH,"test_with_mh.csv")
+VAL_CLAUSES_WITH_MULTI_HOT_PATH = os.path.join(PROCESSED_DATA_DIR_PATH,"val_with_mh.csv")
+
+
+# File paths
+LABEL_LIST_PATH = os.path.join(ARTIFACTS_DIR, "label_list.json")
+LABEL_BINARIZER_PATH = os.path.join(ARTIFACTS_DIR, "label_binarizer.pkl")
+
+TRAIN_PARQUET_PATH = os.path.join(ARTIFACTS_DIR, "train.parquet")
+TEST_PARQUET_PATH = os.path.join(ARTIFACTS_DIR, "test.parquet")
+VAL_PARQUET_PATH = os.path.join(ARTIFACTS_DIR, "val.parquet")
+
+Y_TRAIN_PATH = os.path.join(ARTIFACTS_DIR, "y_train.npy")
+Y_VAL_PATH = os.path.join(ARTIFACTS_DIR, "y_val.npy")
+Y_TEST_PATH = os.path.join(ARTIFACTS_DIR, "y_test.npy")
+
+TFIDF_MODEL_PATH = os.path.join(ARTIFACTS_DIR, "tfidf.pkl")
+X_TRAIN_TFIDF_PATH = os.path.join(ARTIFACTS_DIR, "X_train_tfidf.npz")
+X_VAL_TFIDF_PATH = os.path.join(ARTIFACTS_DIR, "X_val_tfidf.npz")
+X_TEST_TFIDF_PATH = os.path.join(ARTIFACTS_DIR, "X_test_tfidf.npz")
+
+# Tokenized datasets
+TOKENIZED_TRAIN = os.path.join(ARTIFACTS_DIR, "train_tokenized")
+TOKENIZED_VAL = os.path.join(ARTIFACTS_DIR, "val_tokenized")
+TOKENIZED_TEST = os.path.join(ARTIFACTS_DIR, "test_tokenized")
