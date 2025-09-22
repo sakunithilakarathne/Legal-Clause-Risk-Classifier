3a1024a94ec1b93a37c8b65ebe5c2f9a75c18cea
Sakuni Thilakarathne <scsthilakarathne@gmail.com>
2025-09-22 12:13:00 +0530

    Add training scripts for base logistic regression model

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ from src.legal_clause_classifier.preprocessing.data_cleaning import *
 from src.legal_clause_classifier.preprocessing.clause_extraction import *
 from src.legal_clause_classifier.preprocessing.data_splitting import *
 from src.legal_clause_classifier.preprocessing.preprocessing import *
+from src.legal_clause_classifier.training.train_classical_ml import train_logistic
 
 
 def main():
-    raw_data = load_dataset(RAW_DATASET_PATH)
-    X_train_tfidf, X_test_tfidf, X_val_tfidf, train_tokenized, val_tokenized, test_tokenized = preprocessing_pipeline(raw_data)
-    
-    
-
+    # raw_data = load_dataset(RAW_DATASET_PATH)
+    # X_train_tfidf, X_test_tfidf, X_val_tfidf, train_tokenized, val_tokenized, test_tokenized = preprocessing_pipeline(raw_data)
+    train_logistic()
     
 
 
