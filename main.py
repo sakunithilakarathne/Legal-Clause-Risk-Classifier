ee69ec2bb355333e1b6cf508b92d8e61e35000aa
Sakuni Thilakarathne <scsthilakarathne@gmail.com>
2025-09-23 17:20:17 +0530

    Add changes made to tfidf ann testing script

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from src.legal_clause_classifier.preprocessing.clause_extraction import *
 from src.legal_clause_classifier.preprocessing.data_splitting import *
 from src.legal_clause_classifier.preprocessing.preprocessing import *
 from src.legal_clause_classifier.training.train_classical_ml import train_logistic_regression_model
+from src.legal_clause_classifier.testing.test_tfidf_ann import test_tfidf_ann_model
 
 
 def main():
     # raw_data = load_dataset(RAW_DATASET_PATH)
     # X_train_tfidf, X_test_tfidf, X_val_tfidf, train_tokenized, val_tokenized, test_tokenized = preprocessing_pipeline(raw_data)
-    train_logistic_regression_model()
+    #train_logistic_regression_model()
+    test_tfidf_ann_model()
+
     
 
 
